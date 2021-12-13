92b518d73c7e0dc4981cbf1ae42f221564327bc9
Patricia Padilha <patriciapadilhabio@gmail.com>
2021-12-13 17:02:13 -0300

    requisito 10 -ok

--- script.js
+++ script.js
@@ -5,9 +5,13 @@
 let input = document.getElementById('texto-tarefa');
 let toDoButton = document.getElementById('criar-tarefa');
 let listOl = document.getElementById('lista-tarefas');
+let listLi = document.querySelectorAll('li');
+let cleanButton = document.getElementById('apaga-tudo');
 
 getText ()
 changeBackground ()
+itemCompleted ()
+cleanAllButton ()
 
 
 
@@ -36,18 +40,41 @@ function changeBackground () {
     }); 
 }
 
-// função por mudança de class:
+// // função por mudança de class:
 
 // function changeBackground () {
 //     listOl.addEventListener('click' , function (event) { 
-//         let listLi = document.querySelectorAll('li');
-//         for(let index = 0; index < listLi.length; index += 1){
-//             console.log('eventochangeBackground');
-//             document.querySelector('.changeBackground').classList.remove('changeBackground');
+//         if (event.target.className === 'changeBackground') {
+//             event.target.className = '';
+//         } else {
 //             event.target.classList.add('changeBackground');
 //         }
 //     }); 
 // }
+
+
+
+function itemCompleted () {
+    // console.log(listOl);
+    listOl.addEventListener('dblclick' , function (event) {
+        if (event.target.className === 'completed') {
+            // event.target.classList.remove('completed');
+        } else {
+            event.target.classList.add('completed');
+        }
+    });
+}
+
+
+function cleanAllButton () {
+    cleanButton.addEventListener('click' , function () {
+        let listLi = document.querySelectorAll('li');
+        for(let index = 0; index < listLi.length; index += 1){
+            let element = listLi[index];
+            listOl.removeChild(element);
+        }
+    })
+}
 
 
 
