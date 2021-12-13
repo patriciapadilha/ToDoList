6cc821d5f0ca4623cd24bef337cc9a54cdf9edf9
Patricia Padilha <patriciapadilhabio@gmail.com>
2021-12-13 15:51:16 -0300

    requisito 7 - ok

--- script.js
+++ script.js
@@ -5,22 +5,55 @@
 let input = document.getElementById('texto-tarefa');
 let toDoButton = document.getElementById('criar-tarefa');
 let listOl = document.getElementById('lista-tarefas');
-console.log(input);
 
-//5
-// No campo de input será digitado o texto de uma tarefa qualquer e, em seguida, clicar-se-á no botão de criar tarefa. Será verificado que, após o clique, o texto digitado aparece na lista e desaparece do input.
+getText ()
+changeBackground ()
+
+
 
 function getText (){
     toDoButton.addEventListener('click' , function () {
         let toDo = input.value;
         // console.log(toDo);
         let item = document.createElement('li');
+        item.className = 'item';
         item.innerText = toDo;
         listOl.appendChild(item);
+        input.value = '';
     })
-
 }
 
-getText ()
+
+function changeBackground () {
+    listOl.addEventListener('click' , function (event) { 
+        let listLi = document.querySelectorAll('li');
+        for(let index = 0; index < listLi.length; index += 1){
+            if(listLi[index].style.backgroundColor === 'rgb(128, 128, 128)'){
+                listLi[index].style.backgroundColor = 'rgb(255, 255, 255)';
+            }
+        }
+        event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+    }); 
+}
+
+// função por mudança de class:
+
+// function changeBackground () {
+//     listOl.addEventListener('click' , function (event) { 
+//         let listLi = document.querySelectorAll('li');
+//         for(let index = 0; index < listLi.length; index += 1){
+//             console.log('eventochangeBackground');
+//             document.querySelector('.changeBackground').classList.remove('changeBackground');
+//             event.target.classList.add('changeBackground');
+//         }
+//     }); 
+// }
+
+
+
+
+
+
+
 
 
